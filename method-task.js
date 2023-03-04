a2f1716112937eeef1a216633ad6c5cdfcd57e91
kartini <126692640+kartini27@users.noreply.github.com>
2023-03-04 07:25:27 -0800

    Add files via upload

---
+++ method-task.js
@@ -0,0 +1,140 @@
+//method procedure
+// procedure 
+// function variabel 1
+function hello(nama,asal,suku,universitas,jurusan){
+    console.log(
+        'halo perkenalkan saya' + 
+        nama +
+        'dari ' + asal +'termasuk suku' +
+         suku +
+         'batak toba. kuliah di universitas' +
+         universitas +
+         'dan mengambil jurusan' +
+         jurusan +
+        'salam dari saya'
+        );
+    }
+    hello()
+    hello('kartini situmorang', 'samosir', 'batak toba', 'quality','manajemen');
+   
+
+    // function Variabel 2
+    function hello (nama,asal){
+    console.log(
+        'hallo selamat datang' +
+        nama +
+        'di kelas 2a32 manajemen. semoga' +
+        nama +
+        'teman-teman sehat selalu ya'
+    )
+    };
+    hello ('kartini','medan');
+    hello('samosir');
+    
+
+    //function variabel 3
+    function hello (nama,asal){
+        console.log(
+            'hallo apa kabar' +
+            nama +
+            'di '+ asal  +
+            nama +
+            'teman-teman sehat selalu ya'
+        )
+        };
+        hello ('petrus','jakarta');
+        hello('samosir');
+        
+
+        // variabel 4
+     function hello (nama,asal){
+    console.log(
+        'hallo apa kabar' +
+        nama +
+        'di samosir '+ asal  + 
+        nama +
+        'semoga sehat selalu ya'
+    )
+    };
+    hello();
+    hello ('fani', 'samosir');
+    hello('lismalia');
+
+
+
+    //variabel 5
+    function hello (nama,umur,hobby){
+        console.log(
+            'hallo perkenalkan saya' +
+            nama +
+            'berumur'+ umur  + 
+            'dan hobby saya'+
+            hobby + 'salam kenal dari saya'
+         
+        )
+        };
+        hello();
+        hello ('fani', '20 tahun','memasak');
+
+    //variabel 6
+    function hello(nama,NIM,jurusan){
+        console.log(
+           'halo saya' +
+           nama + 'NIM saya' +
+           NIM + 'mengambil jurusan' + jurusan + 
+        'senang bertemu dengan kalian '
+
+        )
+    };
+        hello('sabas','2002020049','manajemen')
+        
+        
+    
+
+
+// get function 
+// get variabel 1
+    function getVariabel1 (nama,asal,suku,universitas,jurusan){
+        console.log(getVariabel1(nama,asal,suku,universitas,jurusan))   
+    };
+   
+
+    //get variabel 2
+    function getVariabel2(nama,asal){
+        console.log(getVariabel2(nama,asal))
+        };
+
+        //get variabel 3
+        funtion getVariabel3(nama,asal){
+            console.log(getVariabel3(nama,asal))
+        };
+
+        //get variabel 4
+function getvariabel4(nama,asal){
+    console.log(getVariabel5(nama,asal))
+
+};
+
+//get variabel 5
+function getVariabel5(nama,umur,hobby){
+    console.log(variabel5(nama,umur,hobby))
+
+};
+
+//get variabel 6
+function getVariabel6(nama,NIM,jurusan){
+    console.log(getVariabel6(nama,NIM,jurusan))
+};
+    
+
+    
+   
+    
+   
+
+    
+
+    
+
+
+
